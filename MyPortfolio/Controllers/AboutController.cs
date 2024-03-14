562c00cc601e6a6fbc3efb5bdd55ffd2214d6f2d
Erhan Gündüz <erhangndz@live.com>
2024-03-14 22:58:52 +0300

    Add project files.

---
+++ MyPortfolio/Controllers/AboutController.cs
@@ -0,0 +1,19 @@
+﻿using MyPortfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolio.Controllers
+{
+    public class AboutController : Controller
+    {
+        MyAcademyPortfolioProjectEntities db = new MyAcademyPortfolioProjectEntities();
+        public ActionResult Index()
+        {
+            var values = db.TblAbouts.ToList();
+            return View(values);
+        }
+    }
+}
---
+++ MyPortfolio/Controllers/AdminLayoutController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolio.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        // GET: AdminLayout
+       
+        public PartialViewResult AdminLayoutSideBar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult AdminLayoutHead()
+        {
+            return PartialView();
+        }
+
+
+    }
+}
