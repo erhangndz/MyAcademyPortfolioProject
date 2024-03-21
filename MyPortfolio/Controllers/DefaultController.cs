7917814b40c1879530d9230ca4a0eff0a6d9bf69
Erhan Gündüz <erhangndz@live.com>
2024-03-21 23:00:24 +0300

    ders 6 Sonu

---
+++ MyPortfolio/Controllers/DefaultController.cs
@@ -0,0 +1,35 @@
+﻿using MyPortfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolio.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        MyAcademyPortfolioProjectEntities db = new MyAcademyPortfolioProjectEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult DefaultFeaturePartial()
+        {
+            var values = db.TblFeatures.ToList();
+
+            return PartialView(values);
+        }
+
+
+        public PartialViewResult DefaultAboutPartial()
+        {
+            var values = db.TblAbouts.ToList();
+
+            return PartialView(values);
+        }
+    }
+}
---
+++ MyPortfolio/Controllers/ProjectController.cs
@@ -0,0 +1,82 @@
+﻿using MyPortfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolio.Controllers
+{
+    public class ProjectController : Controller
+    {
+       MyAcademyPortfolioProjectEntities db = new MyAcademyPortfolioProjectEntities();
+        public ActionResult Index()
+        {
+            var values = db.TblProjects.ToList();
+            return View(values);
+        }
+
+        public ActionResult AddProject()
+        {
+            var categories = db.TblCategories.ToList();
+
+            List<SelectListItem> categoryList= (from x in categories select new SelectListItem
+            {
+                Text= x.CategoryName,
+                Value=x.CategoryId.ToString(),
+                
+            }).ToList();
+
+            ViewBag.category = categoryList;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddProject(TblProjects project)
+        {
+            db.TblProjects.Add(project);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateProject(int id)
+        {
+            var categories = db.TblCategories.ToList();
+
+            List<SelectListItem> categoryList = (from x in categories
+                                                 select new SelectListItem
+                                                 {
+                                                     Text = x.CategoryName,
+                                                     Value = x.CategoryId.ToString(),
+
+                                                 }).ToList();
+
+            ViewBag.category = categoryList;
+            var value = db.TblProjects.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateProject(TblProjects projects)
+        {
+            var value = db.TblProjects.Find(projects.ProjectId);
+            value.ProjectName= projects.ProjectName;
+            value.ImageUrl= projects.ImageUrl;
+            value.GithubUrl=projects.GithubUrl;
+            value.CategoryId=projects.CategoryId;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult DeleteProject(int id)
+        {
+            var value = db.TblProjects.Find(id);
+            db.TblProjects.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
